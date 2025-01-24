b5095849033a692c32b2bafc65398744c14c3c4a
BishalAdhikari0123 <117503902+BishalAdhikari0123@users.noreply.github.com>
2025-01-24 15:49:42 +0545

    add user registration HTML form and update server to serve static files

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-import express from 'express'
-import connectToDB from './connect.js'
-// import requestLogger from './middlewares/requestLogger.js'
+import express from 'express';
+import connectToDB from './connect.js';
+// import requestLogger from './middlewares/requestLogger.js';
 import routes from './routes/index.js';
 import getUserFromAuthToken from './middlewares/getUserFromAuthToken.js';
+import path from 'path'; // Import path module to resolve the directory
 
 let app;
 
+
 connectToDB().then(function (connectMessage) {
-    console.log(connectMessage)
+    console.log(connectMessage);
     app = express();
-    app.use(express.json())
-    app.use(getUserFromAuthToken)
+
+    app.use(express.static(path.join(process.cwd(), 'public')));
+
+    // Middleware to parse incoming JSON data
+    app.use(express.json());
+
+    // Middleware to extract user from auth token
+    app.use(getUserFromAuthToken);
+
+    // Set up routes
     app.use(routes);
-    // app.use(requestLogger)
+
+    // app.use(requestLogger);
+
     const port = process.env.PORT || 4000;
     app.listen(port, function () {
-        console.log("Server running on PORT", port)
-    })
+        console.log("Server running on PORT", port);
+    });
 }).catch(function (err) {
-    console.error(err)
-})
-    
+    console.error(err);
+});
+
 export default app;
